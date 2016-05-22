54efe3862fe688baab681c1ea732618ee5dd5649
hovhannnes <hovhannespost@gmail.com>
2016-05-22 14:15:58 +0400

    remove bower

--- public/admin/dist/bootstrap/config.js
+++
@@ -1,5 +0,0 @@
-angular.module("Art").config(function (localStorageServiceProvider) {
-    localStorageServiceProvider
-        .setPrefix('Art')
-        .setStorageType('sessionStorage');
-});
--- public/admin/dist/factory/Storage.js
+++
@@ -1,3 +0,0 @@
-angular.module("Art").factory("Storage",function(){
-
-});
